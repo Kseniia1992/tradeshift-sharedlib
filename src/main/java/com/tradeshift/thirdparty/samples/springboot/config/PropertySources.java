45328eecaed73d48637a59efd3c0e60085353f40
Kseniia1992 <kseniia.solodovnik@kindgeek.com>
2016-09-08 14:03:24 +0300

    added comment

--- src/main/java/com/tradeshift/thirdparty/samples/springboot/config/PropertySources.java
+++ src/main/java/com/tradeshift/thirdparty/samples/springboot/config/PropertySources.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+// Configuration
 @Configuration(value = "propertySources")
 public class PropertySources {
 
